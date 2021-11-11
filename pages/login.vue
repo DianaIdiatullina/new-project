03f78a5e5e567e39ba136fba3e1a1ea126dcef01
Diana <id789366@gmail.com>
2021-11-11 14:36:33 +0100

    login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative flex flex-col justify-center min-h-screen bg-gray-100 items-center p-8">
-    <h1 class="text-xl font-bold mb-6">Регистрация</h1>
+    <h1 class="text-xl font-bold mb-6">Вход</h1>
 
     <form
       @submit="checkForm"
@@ -13,14 +13,6 @@
           <li v-for="error in errors" class="text-sm">{{ error }}</li>
         </ul>
       </div>
-      <input
-        v-model="name"
-        type="text"
-        placeholder="Name"
-        name="name"
-        required
-        class="block shadow-md p-4 mb-2 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-full"
-      >
       <input
         v-model="email"
         type="text"
@@ -41,7 +33,7 @@
         type="submit"
         class="px-8 py-4 text-white bg-purple-500 hover:bg-purple-700 w-full"
       >
-        Зарегистрироваться
+        Войти
       </button>
     </form>
   </div>
@@ -53,7 +45,6 @@ export default {
   data() {
     return {
       errors: [],
-      name: null,
       email: null,
       password: null,
     }
@@ -62,22 +53,19 @@ export default {
     async checkForm (e) {
       this.errors = [];
 
-      if (!this.name) {
-        this.errors.push('Укажите имя.');
-      }
       if (!this.email) {
         this.errors.push('Укажите электронную почту.');
       } else if (!this.validEmail(this.email)) {
         this.errors.push('Укажите корректный адрес электронной почты.');
       }
+
       if (!this.password) {
         this.errors.push('Укажите пароль.');
-      } else if (!this.validPassword(this.password)) {
-        this.errors.push('Пароль должен содержать буквы (верхнего и нижнего регистра), цифры, спецсимволы. Длина пароля минимум 8 символов.');
       }
 
       if (!this.errors.length) {
-        await this.postFromData()
+        // await this.postFromData()
+        this.postFromData()
         this.$router.push('/')
       }
 
@@ -87,13 +75,8 @@ export default {
       let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
     },
-    validPassword (password) {
-      let re = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g;
-      return re.test(password);
-    },
     async postFromData () {
-      await this.$axios.$post('/api/v1/signup', {
-        name: this.name,
+      await this.$axios.$post('/api/v1/login', {
         email: this.email,
         password: this.password
       })
