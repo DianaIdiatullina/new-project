be1a72d8665884f61ac2dedee6425f96008e6978
Diana <id789366@gmail.com>
2021-11-22 20:14:07 +0100

    vacancies

---
+++ pages/vacancies/_vacancy.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container mx-auto h-full p-4">
+    <h1 class="text-2xl font-bold mb-4">
+      {{ item.title }} {{ item.city }}
+    </h1>
+    <div>{{ item.price }}</div>
+    <div>{{ item.description }}</div>
+    <h3 class="font-bold mt-8">Контакты</h3>
+    <div>{{ item.contact.name }}</div>
+    <div>{{ item.contact.phone }}</div>
+    <div>{{ item.contact.email }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      item:
+        {
+          title: '123',
+          city: 'xvffdxb',
+          key: '123',
+          price: '1778',
+          description: 'Корпоративное обучение. График работы: сменный по 12 часов. Возможность карьерного роста. Стабильная заработная плата + премии.',
+          contact: {
+            name: 'drsfd',
+            phone: '439593945',
+            email: 'drsfdesfs'
+          }
+        }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/vacancies/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mx-auto h-full p-4">
+    <h1 class="text-2xl font-bold mb-4">
+      Вакансии
+    </h1>
+    <nuxt-link
+      v-for="(item, index) in vacancies"
+      :key="index"
+      :to="`/vacancies/${item.key}`"
+      class="block p-4 w-full shadow-lg mb-4"
+    >
+      <h2 class="text-xl font-bold mb-2">
+        {{ item.title }} {{ item.city }}
+      </h2>
+      <div>{{ item.price }}</div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      vacancies: [
+        {
+          title: '123',
+          city: 'xvffdxb',
+          key: '123',
+          price: '1778'
+        },
+        {
+          title: '1234',
+          city: 'xvffdxb',
+          key: '1234',
+          price: '1778'
+        }
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
